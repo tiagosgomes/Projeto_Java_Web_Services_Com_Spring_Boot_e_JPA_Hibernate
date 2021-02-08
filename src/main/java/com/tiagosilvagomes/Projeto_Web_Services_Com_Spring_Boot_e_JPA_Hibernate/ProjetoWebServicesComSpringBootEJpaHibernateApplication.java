84789910bb95af470426b85bddf3b886e3ec8548
tiagosgomes <38902127+tiagosgomes@users.noreply.github.com>
2021-02-08 15:12:32 -0300

    Project Created - O Projeto foi criado!

---
+++ src/main/java/com/tiagosilvagomes/Projeto_Web_Services_Com_Spring_Boot_e_JPA_Hibernate/ProjetoWebServicesComSpringBootEJpaHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.tiagosilvagomes.Projeto_Web_Services_Com_Spring_Boot_e_JPA_Hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoWebServicesComSpringBootEJpaHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoWebServicesComSpringBootEJpaHibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tiagosilvagomes/Projeto_Web_Services_Com_Spring_Boot_e_JPA_Hibernate/ProjetoWebServicesComSpringBootEJpaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiagosilvagomes.Projeto_Web_Services_Com_Spring_Boot_e_JPA_Hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoWebServicesComSpringBootEJpaHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
