93defbd6194c0b6637622e498397306fea856157
tiagosgomes <38902127+tiagosgomes@users.noreply.github.com>
2021-02-09 15:32:45 -0300

    ManyToMany Association With JoinTable

--- src/main/java/com/tiagosilvagomes/Projeto_Web_Services_Com_Spring_Boot_e_JPA_Hibernate/entities/Category.java
+++ src/main/java/com/tiagosilvagomes/Projeto_Web_Services_Com_Spring_Boot_e_JPA_Hibernate/entities/Category.java
@@ -8,21 +8,24 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
-import javax.persistence.Transient;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "tb_category")
-public class Category implements Serializable{
+public class Category implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
-	
-	@Transient
+
+	@JsonIgnore
+	@ManyToMany(mappedBy = "categories")
 	private Set<Product> products = new HashSet<>();
 
 	public Category() {
