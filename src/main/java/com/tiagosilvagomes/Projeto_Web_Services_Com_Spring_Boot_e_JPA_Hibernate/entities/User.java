7b7dda4cd8122944e41243104a3a7dd6a7057761
tiagosgomes <38902127+tiagosgomes@users.noreply.github.com>
2021-02-09 10:37:23 -0300

    Creating Order, Instant and ISO 8601 Parte I

--- src/main/java/com/tiagosilvagomes/Projeto_Web_Services_Com_Spring_Boot_e_JPA_Hibernate/entities/User.java
+++ src/main/java/com/tiagosilvagomes/Projeto_Web_Services_Com_Spring_Boot_e_JPA_Hibernate/entities/User.java
@@ -1,13 +1,18 @@
 package com.tiagosilvagomes.Projeto_Web_Services_Com_Spring_Boot_e_JPA_Hibernate.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
@@ -19,6 +24,9 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	public User() {		
 	}
@@ -70,6 +78,10 @@ public class User implements Serializable {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 	@Override
