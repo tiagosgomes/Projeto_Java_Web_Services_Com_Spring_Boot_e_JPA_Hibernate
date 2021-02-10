18503eb6430360316e2527dc868aff5c1a46deba
tiagosgomes <38902127+tiagosgomes@users.noreply.github.com>
2021-02-10 10:11:02 -0300

    Subtotal and Total Methods Created!

--- src/main/java/com/tiagosilvagomes/Projeto_Web_Services_Com_Spring_Boot_e_JPA_Hibernate/entities/Order.java
+++ src/main/java/com/tiagosilvagomes/Projeto_Web_Services_Com_Spring_Boot_e_JPA_Hibernate/entities/Order.java
@@ -94,20 +94,28 @@ public class Order implements Serializable {
 		return items;
 	}
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
 	public Payment getPayment() {
 		return payment;
 	}
 
 	public void setPayment(Payment payment) {
 		this.payment = payment;
+	}
+
+	public Double getTotal() {
+		double sum = 0.0;
+		for (OrderItem x : items) {
+			sum = sum + x.getSubTotal();
+		}
+		return sum;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
 	@Override
@@ -126,5 +134,4 @@ public class Order implements Serializable {
 			return false;
 		return true;
 	}
-
 }
