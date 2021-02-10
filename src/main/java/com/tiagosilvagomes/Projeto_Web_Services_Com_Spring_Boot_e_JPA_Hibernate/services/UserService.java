305a4ec4c78e79e9b05b7c2440a7d74c9e0ff7f6
tiagosgomes <38902127+tiagosgomes@users.noreply.github.com>
2021-02-10 14:57:13 -0300

    Exception Handling - Update!

--- src/main/java/com/tiagosilvagomes/Projeto_Web_Services_Com_Spring_Boot_e_JPA_Hibernate/services/UserService.java
+++ src/main/java/com/tiagosilvagomes/Projeto_Web_Services_Com_Spring_Boot_e_JPA_Hibernate/services/UserService.java
@@ -3,6 +3,8 @@ package com.tiagosilvagomes.Projeto_Web_Services_Com_Spring_Boot_e_JPA_Hibernate
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -43,9 +45,13 @@ public class UserService {
 	}
 
 	public User update(Long id, User obj) {
-		User entity = userRepository.getOne(id);
-		updateData(entity, obj);
-		return userRepository.save(entity);
+		try {
+			User entity = userRepository.getOne(id);
+			updateData(entity, obj);
+			return userRepository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
